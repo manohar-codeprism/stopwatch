51b8f2eba323a1698b1e6a220bbe7471b9d17d79
manohar-codeprism <manoharthullimalli9@gmail.com>
2021-11-24 11:43:44 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/Components/ButtonComponent.js
    	new file:   src/Components/DisplayComponent.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import DisplayComponent from './Components/DisplayComponent';
+import ButtonComponent from './Components/ButtonComponent';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState({ms:0, s:0, m:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));  
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m;
+
+  const run = () => {
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+     <div className="clock-holder">
+          <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <ButtonComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+     </div>
     </div>
   );
 }
